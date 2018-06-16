ac475d372f983611790e16eb8df028d2164bcf9f
Yo <yoanm@users.noreply.github.com>
2018-06-16 10:47:23 +0200

    Add SchemaTypeNormalizer (#4)

---
+++ src/App/Normalizer/Component/SchemaTypeNormalizer.php
@@ -0,0 +1,49 @@
+<?php
+namespace Yoanm\JsonRpcHttpServerSwaggerDoc\App\Normalizer\Component;
+
+use Yoanm\JsonRpcServerDoc\Domain\Model\Type\TypeDoc;
+
+/**
+ * Class SchemaTypeNormalizer
+ */
+class SchemaTypeNormalizer
+{
+    /**
+     * @private
+     * @type array
+     */
+    const MANAGED_TYPE_LIST = [
+        'array',
+        'number',
+        'object',
+        'string',
+        'integer',
+        'boolean',
+        'null',
+    ];
+    /**
+     * @private
+     * @type array
+     */
+    const RENAMED_TYPE_LIST = [
+        'float' => 'number',
+        'collection' => 'array',
+    ];
+
+    /**
+     * @param TypeDoc $doc
+     *
+     * @return mixed|string
+     */
+    public function normalize(TypeDoc $doc)
+    {
+        $type = str_replace('Doc', '', lcfirst((new \ReflectionClass($doc))->getShortName()));
+        if (in_array($type, self::MANAGED_TYPE_LIST)) {
+            return $type;
+        } elseif (in_array($type, array_keys(self::RENAMED_TYPE_LIST))) {
+            return self::RENAMED_TYPE_LIST[$type];
+        }
+
+        return 'string';
+    }
+}
